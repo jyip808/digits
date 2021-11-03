e1ad1da46928dd6b2e598c8a0dc7bdace52ebada
JustinY <jyip808@hawaii.edu>
2021-11-02 16:11:37 -1000

    contacts collection finished in 38:21 minutes.

--- app/imports/ui/pages/ListContactsAdmin.jsx
+++ app/imports/ui/pages/ListContactsAdmin.jsx
@@ -4,10 +4,10 @@ import { Container, Card, Header, Loader } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Contacts } from '../../api/contacts/Contacts';
-import Contact from '../components/Contact';
+import ContactAdmin from '../components/ContactAdmin';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-class ListContacts extends React.Component {
+class ListContactsAdmin extends React.Component {
 
   // If the subscription(s) have been received, render the page, otherwise show a loading icon.
   render() {
@@ -20,14 +20,14 @@ class ListContacts extends React.Component {
       <Container>
         <Header as="h2" textAlign="center" inverted>List Contacts</Header>
         <Card.Group>
-          {this.props.contacts.map((contact, index) => <Contact key={index} contact={contact}/>)}
+          {this.props.contacts.map((contact, index) => <ContactAdmin key={index} contact={contact}/>)}
         </Card.Group>
       </Container>
     );
   }
 }
 // Require an array of Stuff documents in the props.
-ListContacts.propTypes = {
+ListContactsAdmin.propTypes = {
   contacts: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
@@ -35,11 +35,11 @@ ListContacts.propTypes = {
 // withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Contacts.userPublicationName);
+  const subscription = Meteor.subscribe(Contacts.adminPublicationName);
   // Determine if the subscription is ready
   // Get the Stuff documents
   return {
     contacts: Contacts.collection.find({}).fetch(),
     ready: subscription.ready(),
   };
-})(ListContacts);
+})(ListContactsAdmin);
